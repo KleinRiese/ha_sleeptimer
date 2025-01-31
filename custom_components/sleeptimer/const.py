a9afc6f6415043b399573ecfe53d97408ebba14a
KleinRiese <83984038+KleinRiese@users.noreply.github.com>
2025-01-31 19:00:08 +0100

    dropdown

--- custom_components/sleeptimer/const.py
+++ custom_components/sleeptimer/const.py
@@ -2,6 +2,6 @@ DOMAIN = "sleeptimer"
 DEFAULT_NAME = "SleepTimer"
 DEFAULT_TIMEOUT = 600  # 10 minutes in seconds
 
-CONF_NAME = "Name"
+CONF_NAME = "Name (virtual switch)"
 CONF_ENTITY_ID = "Entity_id (real switch)"
 CONF_TIMEOUT = "Timeout (s)"
