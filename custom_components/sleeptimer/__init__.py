a4e37501a2c5d643d823d6b75adfcb735ccfe641
KleinRiese <83984038+KleinRiese@users.noreply.github.com>
2025-02-07 16:48:28 +0100

    fix

--- custom_components/sleeptimer/__init__.py
+++ custom_components/sleeptimer/__init__.py
@@ -2,7 +2,7 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry):
     """Set up SleepTimer from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(config_entry, "switch")
+    await hass.config_entries.async_forward_entry_setup(config_entry, "switch")
     return True
 
 async def async_unload_entry(hass, config_entry):
